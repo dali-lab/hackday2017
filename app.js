5a054137ed88e86589284f190edd95aa86f03665
Tyler <tyler@burnamtech.com>
2017-04-08 13:15:43 -0400

    refactored to official slack node api

--- app.js
+++ app.js
@@ -4,31 +4,46 @@ if (!process.env.token) {
 }
 
 
-const botkit = require('botkit');
 const os = require('os');
-const controller = botkit.slackbot();
+var RtmClient = require('@slack/client').RtmClient;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var bot = controller.spawn({
-    token: process.env.token
-}).startRTM();
-// reply to non-mentioning incoming messages
-controller.on('ambient', function(bot, message) {
 
-    console.log(message)
-    // This seems to be the message type for "sent messages"
-    if (message['type'] == 'message') {
-        // Echo message
-        bot.reply(message, 'You said: ' + message['text']);
-    }
+// Build client
+var slack = new RtmClient(process.env.token);
+slack.start();
+
+// On any message, do stuff
+slack.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  console.log('Message:', message);
+
+      if (message['type'] == 'message') {
+          slack.sendMessage("I think you said: " + message['text'], message['channel']);
+      }
 });
 
-// reply to messages @hackbot
-controller.on('message_received', function(bot, message) {
-
-    console.log(message)
-    // This seems to be the message type for "sent messages"
-    if (message['type'] == 'desktop_notification') {
-        // Echo message
-        bot.reply(message, "In the future, I will support commands. For now, I'm not listening")
-    }
-});
+//
+// var bot = controller.spawn({
+//     token: process.env.token
+// }).startRTM();
+// // reply to non-mentioning incoming messages
+// controller.on('ambient', function(bot, message) {
+//
+//     console.log(message)
+//     // This seems to be the message type for "sent messages"
+//     if (message['type'] == 'message') {
+//         // Echo message
+//         bot.reply(message, 'You said: ' + message['text']);
+//     }
+// });
+//
+// // reply to messages @hackbot
+// controller.on('message_received', function(bot, message) {
+//
+//     console.log(message)
+//     // This seems to be the message type for "sent messages"
+//     if (message['type'] == 'desktop_notification') {
+//         // Echo message
+//         bot.reply(message, "In the future, I will support commands. For now, I'm not listening")
+//     }
+// });
