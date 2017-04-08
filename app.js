739948122500c4b4520a2275f3d23f15f6dc3274
Tyler <tyler@burnamtech.com>
2017-04-08 12:06:08 -0400

    boilerplate code - slackbot will react to messages

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("one day, I'll be a slack bot")
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+})
